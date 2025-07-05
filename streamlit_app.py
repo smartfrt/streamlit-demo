20c37ff1bcf0b466f5439199bd3911960c9f28c7
FrankWu <frank6wu@gmail.com>
2025-07-05 23:42:49 +0000

    added views for chat history

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
+from st_chat_message import message
 from openai import OpenAI
 
-st.title("Hi there!")
-
-# Open the .env file, and read in the api key into a variable
+# "with": wait for the operation to finish
 with open(".env", "r") as file:
     open_ai_api_key = file.read()
 
@@ -16,18 +15,35 @@ if "chat_history" not in st.session_state:
         {"role": "system", "content": "Please translate all user input into Chinese."}
     ]
 
-# user_input = "I need some help with writing a letter to my professor. Can you help me with that?"
-user_input = st.text_input("Enter your message: ")
-st.session_state["chat_history"].append({"role": "user", "content": user_input})
+# print all messages
+for chat_message in st.session_state["chat_history"]:
+    # check if it's the user's chat message
+    # If so, create a "user message" element
+    if chat_message["role"] == "user" and chat_message["content"] != "":
+        message(chat_message["content"], is_user=True)
+    elif chat_message["role"] == "assistant":
+        message(chat_message["content"])
+    else:
+        continue
 
-# sending a request to OpenAI through the OpenAI API
-response = client.chat.completions.create(
-    model="gpt-4.1",
-    messages=st.session_state["chat_history"], 
-    temperature=0
-)
+with st.form("input"):
+    user_message = st.text_input("Please enter your message: ")
+    submitted_btn = st.form_submit_button('Submit')
+    if submitted_btn and user_message != "":
+        st.session_state["chat_history"].append({"role": "user", "content": user_message})
 
-currResponse = response.choices[0].message.content
-st.session_state["chat_history"].append({"role": "assistant", "content": currResponse})
-st.write(currResponse)
+        # sending a message to OpenAI through the OpenAI API
+        response = client.chat.completions.create(
+            model="gpt-4.1",
+            messages=st.session_state["chat_history"], 
+            temperature=0
+        )
+        chatgpt_message = response.choices[0].message.content
+        st.session_state["chat_history"].append({"role": "assistant", "content": chatgpt_message})
+
+        # refresh our screen
+        st.rerun()
+    
+
+
 
